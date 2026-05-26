f5ead1e23e96de0ebe3abd1931765a9ea8a27af2
maria <150175935+mmmaryae@users.noreply.github.com>
2026-05-26 13:59:27 -0300

    Configura conexão do banco MySQL no InfinityFree

--- config/db.php
+++ config/db.php
@@ -1,16 +1,20 @@
 <?php
-// config/db.php conexão com o banco "minuano" 
+// config/db.php
 
-define('DB_HOST',    'localhost');
-define('DB_NAME',    'minuano');       // nome do banco no database.sql
-define('DB_USER',    'root');          // usuário do MySQL na LocalWeb (ver cPanel)
-define('DB_PASS',    '');              // senha do MySQL na LocalWeb (ver cPanel)
+define('DB_HOST',    'sql311.infinityfree.com');
+define('DB_NAME',    'if0_42026129_minuano');
+define('DB_USER',    'if0_42026129');
+define('DB_PASS',    'Mari280404');
 define('DB_CHARSET', 'utf8mb4');
 
 function getDB(): PDO {
     static $pdo = null;
+
     if ($pdo === null) {
-        $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
+        $dsn = "mysql:host=" . DB_HOST .
+               ";dbname=" . DB_NAME .
+               ";charset=" . DB_CHARSET;
+
         try {
             $pdo = new PDO($dsn, DB_USER, DB_PASS, [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
@@ -22,5 +26,6 @@ function getDB(): PDO {
             die('Erro de conexão com o banco de dados.');
         }
     }
+
     return $pdo;
 }
